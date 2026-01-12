be4f1876bbbd9a45a363f51a6790303814bdff77
muppavarapusivaram <muppavarapusivaram03@gmail.com>
2026-01-13 00:48:51 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -8,5 +9,10 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
    printf("$ ");
 
+char input[100];
+fgets(input, sizeof(input), stdin);
+
+input[strcspn(input, "\n")] = '\0';
+printf("%s: command not found\n", input);
   return 0;
 }
