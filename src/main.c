72b25d656becc5490f7b3499d913f125051ed36f
muppavarapusivaram <muppavarapusivaram03@gmail.com>
2026-01-13 09:44:52 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,14 +5,26 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+char input[100];
   // TODO: Uncomment the code below to pass the first stage
+
+while(1)
+{
    printf("$ ");
 
 char input[100];
-fgets(input, sizeof(input), stdin);
+if(fgets(input, sizeof(input), stdin) == NULL)
+{
+printf("\n");
+break;
+}
 
 input[strcspn(input, "\n")] = '\0';
+if(strlen(input) == 0)
+{
+continue;
+}
 printf("%s: command not found\n", input);
+}
   return 0;
 }
