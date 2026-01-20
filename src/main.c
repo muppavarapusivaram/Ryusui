4ae020aa2ee50211607d70bc9d410553d3763228
muppavarapusivaram <muppavarapusivaram03@gmail.com>
2026-01-20 15:38:13 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,78 +3,77 @@
 #include <string.h>
 #include <stdbool.h>
 #include "builtins.h"
+// REMOVE: #include "path_utils.h" - this file doesn't exist anymore
 
 
 char **parse_input(char *input, int *arg_count) {
-	static char *args[64];
-	int count = 0;
-	char *token = strtok(input, " ");
+    static char *args[64];
+    int count = 0;
+    char *token = strtok(input, " ");
 
 
-	while(token != NULL && count < 63) {
-		args[count++] = token;
-		token = strtok(NULL, " ");
-}
-		args[count] = NULL;
-		*arg_count  = count;
+    while(token != NULL && count < 63) {
+        args[count++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[count] = NULL;
+    *arg_count = count;
 
-	return args;
+    return args;
 }
 
 int execute_builtin(char **args, int arg_count) {
-	if(arg_count == 0) return -1;
+    if(arg_count == 0) return -1;
 
-	if(strcmp(args[0], "exit") == 0) {
-	return -2;
-}
+    if(strcmp(args[0], "exit") == 0) {
+        return -2;
+    }
 
-	for(int i = 0; i < num_builtins(); i++) {
-		if(strcmp(args[0], builtins[i].name) == 0) {
-			return builtins[i].func(args);
-}
-}
+    for(int i = 0; i < num_builtins(); i++) {
+        if(strcmp(args[0], builtins[i].name) == 0) {
+            return builtins[i].func(args);
+        }
+    }
 
-	return -1;
+    return -1;
 }
 
 int main(int argc, char *argv[]) {
+    setbuf(stdout, NULL);
+    char input[100];
 
-	setbuf(stdout, NULL);
-		char input[100];
+    while(1) {
+        printf("$ ");
 
-	while(1) {
-		printf("$ ");
+        if(fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
 
-	if(fgets(input, sizeof(input), stdin) == NULL) {
-		printf("\n");
-		break;
-}
+        input[strcspn(input, "\n")] = '\0';
 
+        if(strlen(input) == 0) {
+            continue;
+        }
 
-	input[strcspn(input, "\n")] = '\0';
+        int arg_count = 0;
+        char input_copy[100];
+        strcpy(input_copy, input);
+        char **args = parse_input(input_copy, &arg_count);
 
-	if(strlen(input) == 0) {
-		continue;
-}
+        if(arg_count ==0) {
+            continue;
+        }
 
-	int arg_count = 0;
-	char input_copy[100];
-	strcpy(input_copy, input);
-	char **args = parse_input(input_copy, &arg_count);
+        int result = execute_builtin(args, arg_count);
 
-	if(arg_count ==0) {
-		continue;
-}
+        if(result == -2) {
+            break;
+        }
+        else if(result == -1) {
+            printf("%s: command not found\n", args[0]);
+        }
+    }
 
-	int result = execute_builtin(args, arg_count);
-
-	if(result == -2) {
-		break;
-}
-	else if(result == -1) {
-		printf("%s: command not found\n", args[0]);
-}
-}
-
-	return 0;
+    return 0;
 }
--- src/type.c
+++ src/type.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include "builtins.h"
 
 int is_builtin_command(const char *cmd_name) {
@@ -22,11 +25,39 @@ int type_builtin(char **args) {
         return 1;
     }
     
-    if(is_builtin_command(args[1])) {  // FIXED: Added closing parenthesis
-        printf("%s is a shell builtin\n", args[1]);  // FIXED: Correct message
-    } else {
-        printf("%s: not found\n", args[1]);  // FIXED: Correct message for not found
+    // 1. Check if it's a builtin
+    if(is_builtin_command(args[1])) {
+        printf("%s is a shell builtin\n", args[1]);
+        return 0;
     }
     
+    // 2. Check if it's an executable in PATH
+    char *path_env = getenv("PATH");
+    if (path_env != NULL) {
+        char *path_copy = strdup(path_env);
+        if (path_copy != NULL) {
+            char *dir = strtok(path_copy, ":");
+            
+            while (dir != NULL) {
+                // Build full path: dir + "/" + command
+                char full_path[1024];
+                snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[1]);
+                
+                // Check if file exists and is executable
+                if (access(full_path, X_OK) == 0) {
+                    printf("%s is %s\n", args[1], full_path);
+                    free(path_copy);
+                    return 0;
+                }
+                
+                dir = strtok(NULL, ":");
+            }
+            
+            free(path_copy);
+        }
+    }
+    
+    // 3. Not found
+    printf("%s: not found\n", args[1]);
     return 0;
 }
